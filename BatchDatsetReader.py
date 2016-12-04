a7bb3d94e96d8291b1b569a798378c9a9736f29d
simpleig <tonghehehe@gmail.com>
2016-12-03 17:00:16 -0800

    fixing bugs

--- BatchDatsetReader.py
+++ BatchDatsetReader.py
@@ -59,7 +59,9 @@ class BatchDatset:
         perm = np.arange(len(self.different_images_buffer))
         np.random.shuffle(perm)
         self.different_images = [self.different_images_buffer[idx] for idx in perm]
+        print("length:%d idx:%d"%(len(self.different_images_buffer),idx))
         self.different_annotations = [self.different_annotations_buffer[idx] for idx in perm]
+        print("length:%d idx:%d"%(len(self.different_annotations_buffer),idx))
 
     def next_image(self,batch_size=1):
         start = self.batch_offset
